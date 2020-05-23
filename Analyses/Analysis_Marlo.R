ae1a7adf0169bc1f68bb08b742021e351fadc2ee
joemarlo <joe.marlo.09@gmail.com>
2020-05-23 17:02:35 -0400

    added cluster evaluation methods, hierarchial, kmeans, and model-based clustering methods. Initiated Rmd

--- Analyses/Analysis_Marlo.R
+++ Analyses/Analysis_Marlo.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(mclust)
 library(NbClust)
+set.seed(44)
 theme_set(theme_minimal())
 
 atus_long <- read_tsv('Data/atus.tsv')
@@ -21,41 +22,25 @@ atus_long %>%
        y = NULL)
 
 # log transform the data
-cats_to_log <- unique(atus_scaled$description)[
-  !(unique(atus_scaled$description) %in% c('Socializing, Relaxing, and Leisure', 'Sleep'))]
-atus_log <- atus_long %>%
-  filter(description %in% cats_to_log) %>% 
-  group_by(description) %>% 
-  mutate(value = log(jitter(value + 0.5, factor = 1))) %>% # added noise to account for log 0
-  ungroup() %>% 
-  bind_rows(
-    atus_long %>%
-      filter(!(description %in% cats_to_log))
-  )
-
-atus_log %>% 
-  ggplot(aes(x = value)) +
-  geom_density() +
-  facet_wrap(~description, scales = "free") + 
-  labs(title = 'Feature densities (un-transformed)', 
-       x = NULL)
-
+cats_to_cube <- unique(atus_long$description)[
+  !(unique(atus_long$description) %in% c('Socializing, Relaxing, and Leisure', 'Sleep'))]
 atus_cube_root <- atus_long %>%
-  filter(description %in% cats_to_log) %>% 
+  filter(description %in% cats_to_cube) %>% 
   group_by(description) %>% 
   mutate(value = value^(1/3)) %>%
   ungroup() %>% 
   bind_rows(
     atus_long %>%
-      filter(!(description %in% cats_to_log))
+      filter(!(description %in% cats_to_cube))
   )
 
 atus_cube_root %>%
   ggplot(aes(x = value)) +
   geom_density() +
   facet_wrap( ~ description, scales = "free") +
-  labs(title = 'Feature densities (cube-transformed)',
-       x = NULL)
+  labs(title = 'Feature densities (cube-root-transformed)',
+       x = NULL,
+       y = NULL)
 
 # check spread of the data
 atus_cube_root %>% 
@@ -84,7 +69,7 @@ atus_scaled %>%
 # pivot wider and turn into matrix
 atus_wide <- atus_scaled %>% 
   pivot_wider(values_from = value, names_from = description) %>% 
-  select(-TUCASEID) %>% 
+  select(-ID) %>% 
   as.matrix()
 
 
@@ -109,29 +94,36 @@ scale_01 <- function(x) (x - min(x)) / (max(x) - min(x))
 # sample using survey weights
 total_rows <- nrow(atus_wide)
 sample_size <- 10000
-rows_to_keep <- sample(1:total_rows, size = sample_size, prob = scale_01(weights$TUFNWGTP), replace = TRUE)
-
-resampled_atus <- atus_wide[rows_to_keep,]
+rows_to_keep <- sample(1:total_rows, size = sample_size, prob = scale_01(demographics$survey_weight), replace = TRUE)
+IDs_kept <- atus_scaled %>% 
+  pivot_wider(values_from = value, names_from = description) %>% 
+  select(ID) %>% 
+  .[rows_to_keep,]
+atus_resampled <- atus_wide[rows_to_keep,]
 
 # pairs plot of resampled data
-as_tibble(resampled_atus) %>% 
+as_tibble(atus_resampled) %>% 
   GGally::ggpairs(mapping = aes(alpha = 0.2))
 
+# correlations
+corr_matrix <- cor(atus_resampled)
+corr_matrix[lower.tri(corr_matrix)] %>% density() %>% plot(main = 'Density of correlations between activities')
+
 # run PCA
-atus_pca <- prcomp(resampled_atus)
+atus_pca <- prcomp(atus_resampled)
 summary(atus_pca)
 
 
 # memory management -------------------------------------------------------
 
-rm(atus_long, atus_scaled, weights)
+rm(atus_scaled, atus_wide, atus_cube_root, corr_matrix, pca_plot, cats_to_cube, total_rows)
 gc()
 
 
 # hierarchical -------------------------------------------------------------
 
 # distance matrix for features
-dist_sc <- dist(resampled_atus, method = 'euclidean')
+dist_sc <- dist(atus_resampled, method = 'euclidean')
 # try single, centroid, and ward (D2) linkage hier clustering
 # hcl_single <- hclust(d = dist_sc, method = 'single')
 # hcl_centroid <- hclust(d = dist_sc, method = 'centroid')
@@ -150,39 +142,124 @@ dev.off()
 # Ward’s minimum variance method, 
 # with dissimilarities are squared before clustering
 dend <- as.dendrogram(hcl_ward)
-hcl_k <- 4
+hcl_k <- 3
 dend_col <- color_branches(dend, k = hcl_k)
 plot(dend_col, main = paste0('Ward (D2) Linkage: K = ', hcl_k))
 
+# examine the demographics through the clusters
+groupings <- cutree(hcl_ward, 3)
+demographics %>% 
+  right_join(bind_cols(IDs_kept, group = groupings)) %>%
+  select(-c('ID', 'survey_weight')) %>%
+  mutate_all(as.character) %>% 
+  pivot_longer(cols = -group) %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram(stat = 'count') +
+  facet_grid(group ~ name, scales = 'free') +
+  labs(title = 'Demographics split by cluster', 
+       x = NULL,
+       y = NULL)
+  
 
 # memory management -------------------------------------------------------
 
-rm(dend, hcl_k, dend_col)
+rm(dist_sc, dend, dend_col, hcl_k, dend_col, groupings, sample_size)
 gc()
 
 
-
-
-# Naive Bayes and SVM -----------------------------------------------------
-
-
-
-
-# clustering --------------------------------------------------------------
+# optimizing hierarchical cluster sizes ------------------------------------------------
 
 # get optimal cluster sizes 
-cluster_sizes_hcl_ch <- NbClust(data = nba_feat_sc,
-                                # it will likely be harder to interpret clusters
-                                # past this amount
-                                max.nc = 6,
-                                method = 'ward.D2',
-                                index = 'ch')
+hcl_ch <- NbClust(
+  data = atus_resampled,
+  max.nc = 14,
+  method = 'ward.D2',
+  index = 'ch'
+)
+hcl_ch$All.index %>% plot(type = 'l', x = 2:14, main = 'Hierarchical: C(g) index', xlab = 'n clusters')
 
 # get optimal cluster sizes 
-cluster_sizes_hcl_s <- NbClust(data = nba_feat_sc,
-                               # it will likely be harder to interpret clusters
-                               # past this amount
-                               max.nc = 6,
-                               method = 'ward.D2',
-                               index = 'silhouette')
+hcl_silhouette <- NbClust(
+  data = atus_resampled,
+  max.nc = 14,
+  method = 'ward.D2',
+  index = 'silhouette'
+)
+hcl_silhouette$All.index %>% plot(type = 'l', x = 2:14, main = 'Hierarchical: Silhouette', xlab = 'n clusters')
+
+
+
+# kmeans ------------------------------------------------------------------
+
+# get optimal cluster sizes 
+km_ch <- NbClust(
+  data = atus_resampled,
+  max.nc = 20,
+  method = 'kmeans',
+  index = 'ch'
+)
+km_ch$All.index %>% plot(type = 'l', x = 2:20, main = 'Kmeans: C(g) index', xlab = 'n clusters')
+
+# get optimal cluster sizes 
+km_silhouette <- NbClust(
+  data = atus_resampled,
+  max.nc = 20,
+  method = 'kmeans',
+  index = 'silhouette'
+)
+km_silhouette$All.index %>% plot(type = 'l', x = 2:20, main = 'Kmeans: Silhouette', xlab = 'n clusters')
+
+# run the final kmeans algo with optimal number of clusters
+km_eleven <- kmeans(x = atus_resampled,
+                  centers = km_ch$Best.nc[[1]],
+                  nstart = 100,
+                  iter.max = 30,
+                  algorithm = 'Hartigan-Wong')
+
+# plot the clusters in PC space
+atus_pca$x[, 1:2] %>% 
+  as_tibble() %>% 
+  mutate(Cluster = as.factor(km_eleven$cluster)) %>% 
+  ggplot(aes(x = PC1, y = PC2, color = Cluster)) +
+  geom_point(alpha = 0.3) +
+  labs(main = 'K-means cluster solution in PC space')
+
+# examine the demographics through the clusters
+demographics %>% 
+  right_join(bind_cols(IDs_kept, cluster = as.factor(km_eleven$cluster))) %>%
+  select(-c('ID', 'survey_weight')) %>%
+  mutate_all(as.character) %>% 
+  pivot_longer(cols = -cluster) %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram(stat = 'count') +
+  facet_grid(cluster ~ name, scales = 'free') +
+  labs(title = 'Demographics split by cluster', 
+       subtitle = 'K-means cluster solution',
+       x = NULL,
+       y = NULL)
+
+
+# model based  clustering -------------------------------------------------
+
+# run model
+mcl <- Mclust(atus_resampled)
+summary(mcl)
+
+# plot 
+plot(mcl, what = "BIC")
+factoextra::fviz_mclust(mcl, "classification", geom = "point", alpha = 0.3)
+
+# examine the demographics through the clusters
+demographics %>% 
+  right_join(bind_cols(IDs_kept, cluster = as.factor(mcl$classification))) %>%
+  select(-c('ID', 'survey_weight')) %>%
+  mutate_all(as.character) %>% 
+  pivot_longer(cols = -cluster) %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram(stat = 'count') +
+  facet_grid(cluster ~ name, scales = 'free') +
+  labs(title = 'Demographics split by cluster', 
+       subtitle = 'Model-based cluster solution',
+       x = NULL,
+       y = NULL)
 
